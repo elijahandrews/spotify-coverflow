f1d8ce6281ce9d7b946426360e11ce8a08d79fc5
Elijah Andrews <elijah.andrews@datadoghq.com>
2022-08-27 13:23:49 -0400

    force auth before opening flask because it doesn't log why it's blocking

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ pip install -r requirements.txt
 # the version in pip is out of date
 pip install git+https://github.com/plamere/spotipy.git --upgrade
 
+# require to create .cache_<USER> file to store spotify credentials
+python -m spotify_coverflow
+
 flask --app web_app run --port=1973 --host=0.0.0.0
